10de17b90d4b404c7b43a1d06200269ccf63b205
Andrew Zito <azito122@gmail.com>
2023-12-26 12:55:13 -0500

    Back to ViewPlugin

--- main.ts
+++ main.ts
@@ -10,9 +10,13 @@ import {
 	EditorView,
 	Decoration,
 	DecorationSet,
+	MatchDecorator,
+	ViewPlugin,
+	ViewUpdate,
 } from "@codemirror/view";
 import { syntaxTree } from "@codemirror/language";
 import { Range, StateField, Transaction, Extension } from '@codemirror/state';
+import { get } from 'http';
 
 interface NiceKBDsSettings {
 	characters: string;
@@ -62,73 +66,57 @@ class NiceKBDsSettingsTab extends PluginSettingTab {
 	}
 }
 
-export const createNiceKBDsEditorExtension = (settings: NiceKBDsSettings) => StateField.define<DecorationSet>({
-	create() {
-		return Decoration.none;
-	},
+const getNiceKBDsViewPlugin = (settings: NiceKBDsSettings) => ViewPlugin.fromClass(class {
+	decorations: DecorationSet;
+	kbdDecorator: MatchDecorator;
 
-	update(prev: DecorationSet, tr: Transaction): DecorationSet {
-		const { state } = tr;
-		const view = state.field(editorEditorField);
+	constructor(view: EditorView) {
+		const characters = settings.characters;
+		const words = settings.words.split(',').join('|');
+		const initialKeyRegex = new RegExp(`([${characters}]+\\w*)|${words}`)
+		const subsequentKeyRegex = new RegExp(`(${initialKeyRegex.source}|\\w)+`)
+		const addKeysRegex = new RegExp(`(?<sep>\\s*\\+\\s*)(?<key>${subsequentKeyRegex.source})`, 'gi')
+		const wholeRegex = new RegExp(`(?<initialKey>${initialKeyRegex.source})(${addKeysRegex.source})*`, 'gi')
 
-		if (view.composing) return prev.map(tr.changes); // User is using IME
+		this.kbdDecorator = new MatchDecorator({
+			regexp: wholeRegex,
+			decorate(add, from, to, match, view) {
+				console.log(from, to)
+				console.log(`Found match: ${match[0]} at ${match.index} to ${match.index + match[0].length}`)
+				add(
+					from,
+					from + (match.groups?.initialKey?.length ?? 0),
+					Decoration.mark({
+						inclusive: true,
+						class: "nice-kbd",
+						tagName: "kbd",
+					})
+				)
 
-		const isSourceMode = !state.field(editorLivePreviewField);
-		if (isSourceMode) return Decoration.none;
-
-		const decorations: Range<Decoration>[] = [];
-
-		for (let { from, to } of view.visibleRanges) {
-			syntaxTree(state).iterate({
-				from,
-				to,
-				enter(node) {
-					const text = state.doc.sliceString(node.from, node.to);
-
-					const indicesForKBDs = new Array<[number, number]>();
-
-					const characters = settings.characters;
-					const words = settings.words.split(',').join('|');
-					const initialKeyRegex = new RegExp(`([${characters}]+\\w*)|${words}`)
-					const subsequentKeyRegex = new RegExp(`(${initialKeyRegex.source}|\\w)+`)
-					const addKeysRegex = new RegExp(`(?<sep>\\s*\\+\\s*)(?<key>${subsequentKeyRegex.source})`, 'gi')
-					const wholeRegex = new RegExp(`(?<initialKey>${initialKeyRegex.source})(${addKeysRegex.source})*`, 'gi')
-
-					// TODO: Don't double up when two initial keys follow each other
-
-					let wholeMatch;
-					while ((wholeMatch = wholeRegex.exec(text))) {
-						indicesForKBDs.push([
-							wholeMatch.index,
-							wholeMatch.index + (wholeMatch.groups?.initialKey?.length ?? 0)
-						]);
-
-						let addKeysMatch;
-						while ((addKeysMatch = addKeysRegex.exec(wholeMatch[0]))) {
-							indicesForKBDs.push([
-								wholeMatch.index + addKeysMatch.index + (addKeysMatch.groups?.sep?.length ?? 0),
-								wholeMatch.index + addKeysMatch.index + addKeysMatch[0].length
-							]);
-						}
-					}
-
-					for (const [start, end] of indicesForKBDs) {
-						decorations.push(Decoration.mark({
+				let addKeysMatch;
+				while ((addKeysMatch = addKeysRegex.exec(match[0]))) {
+					add(
+						from + addKeysMatch.index + (addKeysMatch.groups?.sep?.length ?? 0),
+						from + addKeysMatch.index + addKeysMatch[0].length,
+						Decoration.mark({
 							inclusive: true,
 							class: "nice-kbd",
 							tagName: "kbd",
-						}).range(from + start, from + end))
-					}
+						})
+					)
 				}
-			})
-		}
 
-		return Decoration.set(decorations, true);
-	},
+			}
+		})
 
-	provide(field: StateField<DecorationSet>): Extension {
-		return EditorView.decorations.from(field);
+		this.decorations = this.kbdDecorator.createDeco(view);
 	}
+
+	update(update: ViewUpdate) {
+		this.decorations = this.kbdDecorator.updateDeco(update, this.decorations);
+	}
+}, {
+  decorations: instance => instance.decorations,
 })
 
 export default class NiceKBDsPlugin extends Plugin {
@@ -138,7 +126,7 @@ export default class NiceKBDsPlugin extends Plugin {
 		await this.loadSettings();
 		this.addSettingTab(new NiceKBDsSettingsTab(this.app, this));
 
-		this.registerEditorExtension(createNiceKBDsEditorExtension(this.settings));
+		this.registerEditorExtension(getNiceKBDsViewPlugin(this.settings))
 	}
 
 	onunload() {
